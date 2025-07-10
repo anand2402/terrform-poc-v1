bb6917ddfc2cea4c314f575c393b2bde564369d3
Anand <anand.vijayan@tandg.global>
2025-07-11 10:30:59 +1200

    Add Config, Database and Warehouse

--- tf_poc.tf
+++ tf_poc.tf
@@ -20,3 +20,20 @@ provider "snowflake" {
     authenticator     = "SNOWFLAKE_JWT"
     private_key       = file(local.private_key_path)
 }
+
+resource "snowflake_database" "tf_db" {
+  name         = "TF_DEMO_DB"
+  is_transient = false
+}
+
+resource "snowflake_warehouse" "tf_warehouse" {
+  name                      = "TF_DEMO_WH"
+  warehouse_type            = "STANDARD"
+  warehouse_size            = "XSMALL"
+  max_cluster_count         = 1
+  min_cluster_count         = 1
+  auto_suspend              = 60
+  auto_resume               = true
+  enable_query_acceleration = false
+  initially_suspended       = true
+}
